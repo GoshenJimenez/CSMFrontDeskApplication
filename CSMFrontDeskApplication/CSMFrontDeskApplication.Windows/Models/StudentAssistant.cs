9be68b533a044308457ac8cbe39cef1209e3116d
Ivan Joshua Punzalan Arsua <iparsua@csmbataan.ph.education>
2021-03-14 12:46:12 -0700

    Added a Folder named StudentAssistants and Added a class in the Models folder named StudentsAssistants

---
+++ CSMFrontDeskApplication/CSMFrontDeskApplication.Windows/Models/StudentAssistant.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMFrontDeskApplication.Windows.Models
+{
+    public class StudentAssistant
+    {
+        public string StudentName { get; set; }
+    }
+}

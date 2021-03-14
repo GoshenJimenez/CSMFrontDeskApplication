700f356df60ad89a771256a5ea196357ad652054
Nicole Luzano <nmluzano@csmbataan.ph.education>
2021-03-14 14:55:19 +0800

    Added folder named FAQEntries and added a class named FAQEnties in Model Folder

---
+++ CSMFrontDeskApplication/CSMFrontDeskApplication.Windows/Models/FAQEntries.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMFrontDeskApplication.Windows.Models
+{
+    class FAQEntries
+    {
+    }
+}

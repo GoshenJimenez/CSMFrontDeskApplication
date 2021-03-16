ca760f8e26f153a8576a1495c6e2fe751952ed90
Ivan Joshua Punzalan Arsua <iparsua@csmbataan.ph.education>
2021-03-16 16:24:47 -0700

    Added a code to the StudentAssistant class and added a new class with code included named StudentAssistantLog

--- CSMFrontDeskApplication/CSMFrontDeskApplication.Windows/Models/StudentAssistant.cs
+++ CSMFrontDeskApplication/CSMFrontDeskApplication.Windows/Models/StudentAssistant.cs
@@ -8,6 +8,11 @@ namespace CSMFrontDeskApplication.Windows.Models
 {
     public class StudentAssistant
     {
-        public string StudentName { get; set; }
+        public Guid? Id { get; set; }
+        public string PersonName { get; set; }
+        public string Course { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set;  }
+
     }
 }
---
+++ CSMFrontDeskApplication/CSMFrontDeskApplication.Windows/Models/StudentAssistantLog.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMFrontDeskApplication.Windows.Models
+{
+    public class StudentAssistantLog
+    {
+        public Guid? Id { get; set; }
+        public int StudentAssistantId { get; set; }
+        public DateTime Login { get; set; }
+        public int Hour
+        {
+            get
+            {
+                return this.Login.Hour;
+            }
+        }
+        public int Minute
+        {
+            get
+            {
+                return this.Login.Minute;
+            }
+        }
+        public int Second
+        {
+            get
+            {
+                return this.Login.Second;
+            }
+        }
+        
+        public DateTime Logout { get; set; }
+        public int Hours
+        {
+            get
+            {
+                return this.Logout.Hour;
+            }
+        }
+        public int Minutes
+        {
+            get
+            {
+                return this.Logout.Minute;
+            }
+        }
+        public int Seconds
+        {
+            get
+            {
+                return this.Logout.Second;
+            }
+        }
+
+    }
+}

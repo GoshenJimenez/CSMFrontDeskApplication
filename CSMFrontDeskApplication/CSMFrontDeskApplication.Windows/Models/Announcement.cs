69dd975d8a71b68a9c1c9759dfec04c9c84cdff2
John Kenneth Romero <jeromero@csmbataan.ph.education>
2021-03-16 17:38:19 +0400

    Added a code to the SchoolLocation class and Announcement class

---
+++ CSMFrontDeskApplication/CSMFrontDeskApplication.Windows/Models/Announcement.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMFrontDeskApplication.Windows.Models
+{
+     public class Announcement
+    {
+        public Guid? Id { get; set; }
+
+        public string Content { get; set; }
+
+        public string AnnouncedBy { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public int Month
+        {
+            get
+            {
+                return this.StartDate.Month;
+            }
+        }
+
+        public int Day
+        {
+            get
+            {
+                return this.StartDate.Day;
+            }
+        }
+
+        public int Year
+        {
+            get
+            {
+                return this.StartDate.Year;
+            }
+        }
+        public DateTime EndDate { get; set; }
+        public int Months
+        {
+            get
+            {
+                return this.EndDate.Month;
+            }
+        }
+
+        public int Days
+        {
+            get
+            {
+                return this.EndDate.Day;
+            }
+        }
+
+        public int Years
+        {
+            get
+            {
+                return this.EndDate.Year;
+            }
+        }
+        public int isPublished { get; set; } 
+    }
+}
